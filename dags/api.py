b96a7c0015cf37e85a16453a03592d7d02da4fac
Ken Van Haren <kenvanharen@gmail.com>
2020-11-02 11:00:07 -0800

    API addition

--- dags/api.py
+++ dags/api.py
@@ -9,6 +9,7 @@ from .core.environment import Environment, current_env
 from .core.graph import Graph
 from .core.node import Node
 from .core.pipe import Pipe, pipe
+from .core.runnable import PipeContext
 from .core.sql.pipe import sql_pipe
 from .core.storage.storage import Storage
 from .core.typing.object_schema import ObjectSchema
