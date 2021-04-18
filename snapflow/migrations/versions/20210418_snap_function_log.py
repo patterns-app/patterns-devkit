47d50a11e753e0653853e3f0068291ae28ed4e76
Ken Van Haren <kenvanharen@gmail.com>
2021-04-18 14:03:04 -0700

    Function alembic migrations

---
+++ snapflow/migrations/versions/20210418_snap_function_log.py
@@ -0,0 +1,26 @@
+"""Snap -> function
+
+Revision ID: 3cbdd93d0687
+Revises: fcfed9f36be4
+Create Date: 2021-04-18 13:35:05.061458
+
+"""
+from alembic import op
+import sqlalchemy as sa
+import snapflow
+from sqlalchemy.dialects import postgresql
+
+# revision identifiers, used by Alembic.
+revision = "20210418"
+down_revision = "3cbdd93d0687"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.rename_table("_snapflow_snap_log", "_snapflow_function_log")
+
+
+def downgrade():
+    op.rename_table("_snapflow_function_log", "_snapflow_snap_log")
+
