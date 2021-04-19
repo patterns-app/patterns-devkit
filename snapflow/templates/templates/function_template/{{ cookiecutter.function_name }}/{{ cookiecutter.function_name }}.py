5028b1eebe998f92cfb566b209a3d32b047a672b
Ken Van Haren <kenvanharen@gmail.com>
2021-04-19 14:45:47 -0700

    Update template

--- snapflow/templates/templates/function_template/{{ cookiecutter.function_name }}/{{ cookiecutter.function_name }}.py
+++ snapflow/templates/templates/function_template/{{ cookiecutter.function_name }}/{{ cookiecutter.function_name }}.py
@@ -3,10 +3,12 @@ from __future__ import annotations
 from snapflow import DataBlock, Function, FunctionContext
 
 
-# Add inputs and params
-# @Input("input", stream=True)
-# @Param("param1", datatype="int")
 @Function(namespace="{{ cookiecutter.namespace }}")
-def {{ cookiecutter.function_name }}(ctx: FunctionContext, input: DataBlock):
+def {{ cookiecutter.function_name }}(
+    ctx: FunctionContext,
+    input: DataBlock
+    # ref: Reference   # A reference input
+    # param1: str = "default val"  # A parameter with default
+):
     df = input.as_dataframe() # Or .as_records()
     return df
