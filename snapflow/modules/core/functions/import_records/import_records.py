9dfbd96e062fe51ba230f2d96e6ef13d75c08d91
Ken Van Haren <kenvanharen@gmail.com>
2021-05-05 13:41:50 -0700

    Handle json in import records

--- snapflow/modules/core/functions/import_records/import_records.py
+++ snapflow/modules/core/functions/import_records/import_records.py
@@ -1,16 +1,15 @@
 from __future__ import annotations
 
+import json
 from typing import Optional
 
 from dcp.data_format.formats.memory.records import RecordsFormat
 from snapflow.core.execution.execution import DataFunctionContext
-from snapflow.core.function import Input, Output, Param, datafunction
-from snapflow.core.streams import Stream
+from snapflow.core.function import datafunction
 
 
 @datafunction(
-    namespace="core",
-    display_name="Import Records (List of dicts)",
+    namespace="core", display_name="Import Records (List of dicts)",
 )
 def import_records(
     ctx: DataFunctionContext, records: str, schema: Optional[str] = None
@@ -20,4 +19,6 @@ def import_records(
         # Just emit once
         return  # TODO: typing fix here?
     ctx.emit_state_value("imported", True)
+    if isinstance(records, str):
+        records = json.loads(records)
     ctx.emit(records, data_format=RecordsFormat, schema=schema)
