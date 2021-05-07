6c89b5b6bee31f8bcbe4cef9dbe9f2c54b90e348
Ken Van Haren <kenvanharen@gmail.com>
2021-05-06 17:04:17 -0700

    Context name

--- snapflow/templates/templates/function_template/{{ cookiecutter.function_name }}/{{ cookiecutter.function_name }}.py
+++ snapflow/templates/templates/function_template/{{ cookiecutter.function_name }}/{{ cookiecutter.function_name }}.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
 
-from snapflow import DataBlock, datafunction, DataFunctionContext
+from snapflow import DataBlock, datafunction, Context
 
 
 @datafunction(namespace="{{ cookiecutter.namespace }}")
 def {{ cookiecutter.function_name }}(
-    ctx: DataFunctionContext,
+    ctx: Context,
     input: DataBlock
     # ref: Reference   # A reference input
     # param1: str = "default val"  # A parameter with default
