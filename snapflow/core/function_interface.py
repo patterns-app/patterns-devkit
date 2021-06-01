5b84601f23b74a356155aa6a4b20ea4b5ac0d55f
Ken Van Haren <kenvanharen@gmail.com>
2021-05-31 19:37:21 -0700

    Backwards compatibility

--- snapflow/core/function_interface.py
+++ snapflow/core/function_interface.py
@@ -4,6 +4,7 @@ import inspect
 import re
 from dataclasses import asdict, dataclass, field
 from enum import Enum
+from snapflow.core.data_block import Reference, SelfReference, DataBlock, Consumable
 from typing import TYPE_CHECKING, Any, Dict, List, Optional, Union
 
 from commonmodel.base import SchemaLike
@@ -35,12 +36,7 @@ re_output_type_hint = re.compile(
 
 
 def normalize_parameter_type(pt: str) -> str:
-    return dict(
-        text="str",
-        boolean="bool",
-        number="float",
-        integer="int",
-    ).get(pt, pt)
+    return dict(text="str", boolean="bool", number="float", integer="int",).get(pt, pt)
 
 
 @dataclass
@@ -103,10 +99,7 @@ def parse_output_annotation(a: str) -> ParsedAnnotation:
 
 
 DEFAULT_INPUT_ANNOTATION = "DataBlock"
-DEFAULT_OUTPUT = DataFunctionOutputCfg(
-    schema_key="Any",
-    name=DEFAULT_OUTPUT_NAME,
-)
+DEFAULT_OUTPUT = DataFunctionOutputCfg(schema_key="Any", name=DEFAULT_OUTPUT_NAME,)
 DEFAULT_OUTPUTS = {DEFAULT_OUTPUT_NAME: DEFAULT_OUTPUT}
 DEFAULT_STATE_OUTPUT_NAME = "state"
 DEFAULT_STATE_OUTPUT = DataFunctionOutputCfg(
@@ -181,10 +174,7 @@ def function_interface_from_callable(
             p = parameter_from_annotation(parsed, name=name, default=default)
             params[p.name] = p
         else:
-            i = function_input_from_annotation(
-                parsed,
-                name=param.name,
-            )
+            i = function_input_from_annotation(parsed, name=param.name,)
             inputs[i.name] = i
     return DataFunctionInterfaceCfg(
         inputs=inputs, outputs=outputs, parameters=params, uses_context=uses_context
@@ -204,10 +194,7 @@ def function_input_from_parameter(param: inspect.Parameter) -> DataFunctionInput
             annotation = DEFAULT_INPUT_ANNOTATION
     # is_optional = param.default != inspect.Parameter.empty
     parsed = parse_input_annotation(annotation)
-    return function_input_from_annotation(
-        parsed,
-        name=param.name,
-    )
+    return function_input_from_annotation(parsed, name=param.name,)
 
 
 def function_input_from_annotation(
