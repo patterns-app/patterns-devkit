4bdfc4b968585c8e706ac1c1af2b63a6fc60b336
Ken Van Haren <kenvanharen@gmail.com>
2021-06-27 15:45:44 -0500

    Add example docstring to template

--- snapflow/templates/templates/function_template/{{ cookiecutter.function_name }}/{{ cookiecutter.function_name }}.py
+++ snapflow/templates/templates/function_template/{{ cookiecutter.function_name }}/{{ cookiecutter.function_name }}.py
@@ -13,5 +13,19 @@ def {{ cookiecutter.function_name }}(
     # ref: Reference   # A reference input
     # param1: str = "default val"  # A parameter with default
 ):
+    """Example docstring format. Short description goes here.
+
+    This is the long description. *Accepts
+    [markdown](https://daringfireball.net/projects/markdown/) formatting*.
+
+    Inputs:
+        input: Description of this input
+    
+    Params:
+        param1: Help text for this param
+
+    Output:
+        Description of output
+    """
     df = input.as_dataframe() # Or .as_records()
     return df
