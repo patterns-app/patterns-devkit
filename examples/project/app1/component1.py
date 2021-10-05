29d1c3931284dc41909434b3f5b31983597bbfe7
Ken Van Haren <kenvanharen@gmail.com>
2021-10-05 14:07:23 -0700

    Updates

--- examples/project/app1/component1.py
+++ examples/project/app1/component1.py
@@ -1,12 +1,39 @@
-from basis import component, Table, Record, Parameter, Secret, Context
+from basis import component, Table, Stream, Context
 
+
+def comp():
+    ctx.append_records_to_table(txs)
 
 @component(
-    inputs=[Table("txs", schema="Transaction")],
-    outputs=[Table()],
+    inputs=[Stream("source1_transactions"), Table("customer_summary")],
+    outputs=[Table("customer_sales"), Stream("customer_sales_stream")],
+    node_params=[]
 )
 def component1(ctx: Context):
-    txs_table = ctx.get_table("txs")
-    txs_df = txs_table.as_dataframe()
-    customer_sales_df = txs_df.group_by("customer").sum("amount").reindex()
-    ctx.emit_table(customer_sales_df)
+    cust_table = ctx.get_table("customer_summary")
+    cust_df = cust_table.as_dataframe()
+    for record in ctx.get_records("source1_transactions"):
+        # TODO:
+        cust_ltv = cust_table.execute_sql(f"select ltv from customer_summary where customer_id = {record['customer_id']}")
+        cust_ltv = cust_df.find(record["customer_id"])["ltv"]
+        record["ltv"] = cust_ltv
+        ctx.emit_record(record)
+        ctx.save_progress("source1_transactions")
+    # txs_df = txs_table.as_dataframe()
+    # customer_sales_df = txs_df.group_by("customer").sum("amount").reindex()
+    # ctx.write_table("customer_sales", customer_sales_df)
+
+
+    # ctx.stream_records("customer_sales_stream", customer_sales_df)
+    # ctx.append_to_table("customer_sales", single_record)
+
+    # for record in ctx.get_records("txs"):
+    #     new_record = do_something(record)
+    #     ctx.emit_record(new_record)
+
+
+@simple_streaming_component
+def fn(source1_transactions, customer_summary):
+    # customer_summary.as_dataframe()
+    record["new_val"] = 1
+    return record
--- examples/project/app1/component2.py
+++ examples/project/app1/component2.py
@@ -1,9 +1,9 @@
 import random
 
-from basis import streaming_simple_component
+from basis import simple_streaming_component
 
 
-@streaming_simple_component
+@simple_streaming_component
 def component2(input_record):
     input_record["new_value"] = random.random()
     return input_record
