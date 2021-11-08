66e7acae85ffea24bdae6285daee658e2a11490f
Ken Van Haren <kenvanharen@gmail.com>
2021-11-08 13:10:21 -0800

    Node def format exploration

--- examples/test_graph/import_transactions/import_transactions.py
+++ examples/test_graph/import_transactions/import_transactions.py
@@ -1,8 +1,23 @@
-from collections import OrderedDict
-
-from basis.node.node import node
-
+from basis import node, table
 
 @node
 def node_function_testpy(ctx):
-    pass
+    my_input_table = input_table('clean_transactions.step1', schema='common.Transaction')
+    my_input_df = my_input_table.as_dataframe()
+    my_output_table = output_table('node_function_testpy.stream')
+    my_output_table.write(my_input_df)
+  
+
+@node
+def node_function_testpy(ctx, inputs, outputs, parameters):
+    my_input_df = inputs.my_input_table.as_dataframe() inputs.get("my_input_table").as_records()
+    other_thing.write(my_input_df)
+
+@node
+def node_function_testpy(ctx):
+    my_input_df = ctx.get_input_table("my_input_table").as_dataframe()
+
+
+select
+    *
+from my_input_table
