dee23b75cac7605e782039c775324e3c5db708cd
Ken Van Haren <kenvanharen@gmail.com>
2021-11-11 16:15:52 -0800

    Updated test graph spec

--- tests/test_graph/random_source_floats/random_source_floats.py
+++ tests/test_graph/random_source_floats/random_source_floats.py
@@ -3,10 +3,24 @@ import random
 from basis import table, stream, input, output
 
 
-random_floats = stream("random_floats")
+random_floats = stream("random_floats", schema=MySchema)
+output_table = table("random_floats")
+limit = parameter("limit", "int", default=100)
 
 
 @output(random_floats)
+@parameter(limit)
 def random_source_floats():
-    records = [{"a": random.random() * 100} for _ in range(100)]
+    records = [{"a": random.random() * limit.get_value()} for _ in range(100)]
+    for record in random_floats.get_records():
+        # Do something
     random_floats.append_records(records)
+    df = input_table.as_dataframe()
+    df = input_table.as_records()
+    df = input_table.as_sql("select * from self")
+    output_table.write_dataframe(df)
+    output_table.write_records(records)
+
+    state = get_state()
+    update_state(new_state)
+    error_stream.append_error(myerror)
--- tests/test_graph/summary/summary.sql
+++ tests/test_graph/summary/summary.sql
@@ -1,4 +1,18 @@
-create table {{ table("summary_table") }} as
-select
-    *
-from {{ stripe_charges_table }}
+    with mycte as (
+        
+        )
+
+    create table {{ table("summary_table") }} as
+select * from (
+                  select
+
+
+                  from {{ table("random_floats", schema="Transaction") }} as floats
+join {{ table("other_outputtable") }} as other
+                  on floats.customer_id = other.id
+                      window
+                  where
+                      limit {{ parameter("limit", "int", default=100) }}
+                        
+                  order by
+              )
