81beb317d056daf3f3b68eea8ff48631197c9d2a
AJ <aj@alt>
2021-11-22 11:36:43 -0800

    Fix imports

--- basis/__init__.py
+++ basis/__init__.py
@@ -6,6 +6,5 @@ from .configuration.graph import (
     GraphDefinitionCfg,
     NodeCfg,
 )
-from .execution.context import Context
 from .graph.builder import graph_manifest_from_yaml
 from .graph.configured_node import ConfiguredNode, GraphManifest
