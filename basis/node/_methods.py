2062e1f8a2eec6912c4670d03773163ef1242f91
Ken Van Haren <kenvanharen@gmail.com>
2021-12-07 09:45:33 -0800

    Protocol api methods

--- basis/node/_methods.py
+++ basis/node/_methods.py
@@ -1,15 +1,30 @@
-from typing import List, Iterator
+from __future__ import annotations
+
+from pandas import DataFrame
+from typing import Iterator, Any
 
 
 class InputTableMethods:
     @classmethod
-    def as_dataframe(cls):
+    def as_dataframe(cls) -> DataFrame:
+        ...
+
+    @classmethod
+    def as_records(cls) -> list[dict]:
+        ...
+
+    @classmethod
+    def sql(cls, sql: str) -> InputTableMethods:
+        ...
+
+    @classmethod
+    def chunks(cls, chunk_size: int) -> InputTableMethods:
         ...
 
 
 class OutputTableMethods:
     @classmethod
-    def write(cls, data: List[dict]):
+    def create_table(cls, data: list[dict] | DataFrame):
         ...
 
 
@@ -18,24 +33,37 @@ class InputStreamMethods:
     def records(cls) -> Iterator[dict]:
         ...
 
-
-class OutputStreamMethods:
-    @classmethod
-    def write_records(cls):
+    def __iter__(self) -> Iterator[dict]:
         ...
 
 
-class ParameterMethods:
+class OutputStreamMethods:
     @classmethod
-    def get(cls):
+    def append_record(cls, record: dict):
+        ...
+
+    @classmethod
+    def append_records(cls, records: list[dict]):
         ...
 
 
 class StateMethods:
     @classmethod
-    def records(cls) -> Iterator[dict]:
+    def set_state(cls, state: dict):
         ...
 
     @classmethod
-    def write_records(cls):
+    def set_state_value(cls, key: str, value: Any):
         ...
+
+    @classmethod
+    def get_state(cls) -> dict:
+        ...
+
+    @classmethod
+    def get_state_value(cls, key: str) -> Any:
+        ...
+
+
+class ParameterMethods:
+    pass
