02091344a386367360feaa68a52cba951402f168
AJ <aj@alt>
2021-12-13 12:05:30 -0800

    Fix typing bug with NodeIds

--- basis/graph/builder.py
+++ basis/graph/builder.py
@@ -84,7 +84,7 @@ class _GraphBuilder:
         )
 
     def _err(self, node_or_id: Union[ConfiguredNode, NodeId], message: str):
-        id = node_or_id if isinstance(node_or_id, NodeId) else node_or_id.id
+        id = node_or_id.id if isinstance(node_or_id, ConfiguredNode) else NodeId(node_or_id)
         self.errors.append(GraphError(node_id=id, message=message))
 
     def _read_graph_yml(self, path: Path) -> GraphDefinitionCfg:
@@ -335,7 +335,7 @@ class _GraphBuilder:
     ) -> ConfiguredNode:
         node_file_path = node_dir / node.node_file
         node_name = node.name or self._default_node_name(node_file_path)
-        node_id = node.id or NodeId.from_name(node_name, parent)
+        node_id = NodeId(node.id) if node.id else NodeId.from_name(node_name, parent)
         relative_node_path = self._relative_path_str(node_file_path)
         if node_id in self.nodes_by_id:
             raise ValueError(f"Duplicate node id: {node_id}")
