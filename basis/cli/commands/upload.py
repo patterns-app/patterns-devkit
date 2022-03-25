8e8823b290eb2db8b56b0613c9c7b44fd8773c4b
Ken Van Haren <kenvanharen@gmail.com>
2022-03-25 16:05:01 -0700

    Do not deploy if publishing component (#152)
    Also shows graph errors to user when uploading

--- basis/cli/commands/upload.py
+++ basis/cli/commands/upload.py
@@ -33,12 +33,12 @@ def upload(
         resp = upload_graph_version(ids.graph_file_path, ids.organization_id)
     graph_version_id = resp["uid"]
     ui_url = resp["ui_url"]
+    manifest = resp["manifest"]
     sprint(f"\n[success]Uploaded new graph version with id [b]{graph_version_id}")
-
-    if deploy:
-        with abort_on_error("Deploy failed"):
-            deploy_graph_version(graph_version_id, ids.environment_id)
-        sprint(f"[success]Graph deployed")
+    if manifest.get("errors"):
+        sprint(f"[error]Graph contains the following errors:")
+        for error in manifest["errors"]:
+            sprint(f"\t[error]{error}")
 
     if publish_component:
         with abort_on_error("Error creating component"):
@@ -53,5 +53,10 @@ def upload(
                 f"with versions [b]{resp_versions}[/b] "
                 f"at id [b]{resp_id}"
             )
+
+    elif deploy:
+        with abort_on_error("Deploy failed"):
+            deploy_graph_version(graph_version_id, ids.environment_id)
+        sprint(f"[success]Graph deployed")
 
     sprint(f"\n[info]Visit [code]{ui_url}[/code] to view your graph")
