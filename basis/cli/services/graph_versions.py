e0d744293336a93782f1334f9b4a5cf0e49e5f3a
AJ <aj@alt>
2022-03-28 15:48:33 -0700

    Fix tests

--- basis/cli/services/graph_versions.py
+++ basis/cli/services/graph_versions.py
@@ -7,10 +7,6 @@ def get_graph_by_name(organization_uid: str, name: str) -> dict:
     return get_json(Endpoints.graph_by_name(organization_uid, name))
 
 
-def get_graph_by_id(graph_uid: str) -> dict:
-    return get_json(Endpoints.graph_by_id(graph_uid))
-
-
 def get_graph_version_by_id(graph_version_uid) -> dict:
     return get_json(Endpoints.graph_version_by_id(graph_version_uid))
 
