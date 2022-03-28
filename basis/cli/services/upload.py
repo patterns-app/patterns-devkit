3e5503d431a4c594228cbf0ab42b25ed39e7042d
AJ <aj@alt>
2022-03-28 13:33:14 -0700

    Don't set node ids on ocmponent upload

--- basis/cli/services/upload.py
+++ basis/cli/services/upload.py
@@ -5,8 +5,12 @@ from basis.cli.services.api import Endpoints, post_for_json
 from basis.configuration.edit import GraphDirectoryEditor
 
 
-def upload_graph_version(graph_yaml_path: Path, organization_uid: str) -> dict:
-    editor = GraphDirectoryEditor(graph_yaml_path).add_missing_node_ids()
+def upload_graph_version(
+    graph_yaml_path: Path, organization_uid: str, add_missing_node_ids: bool
+) -> dict:
+    editor = GraphDirectoryEditor(graph_yaml_path)
+    if add_missing_node_ids:
+        editor.add_missing_node_ids()
     return post_for_json(
         Endpoints.graph_version_create(organization_uid),
         data={"payload": json.dumps({"slug": editor.graph_slug()})},
