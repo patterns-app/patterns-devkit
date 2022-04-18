2950b1469be9bcc9b953ea9b3b8830f85fe958d0
Ken Van Haren <kenvanharen@gmail.com>
2022-04-18 15:31:08 -0700

    Update protocol methods (#165)

--- basis/node/_methods.py
+++ basis/node/_methods.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from dataclasses import dataclass
 from typing import Iterator, Any
 
 try:
@@ -9,35 +10,69 @@ except ImportError:
     DataFrame = None
 
 
+@dataclass
+class Table:
+    name: str
+
+
 class InputTableMethods:
     @classmethod
-    def as_dataframe(cls, chunksize: int | None = None) -> DataFrame:
+    def read(
+        cls,
+        as_format: str = "records",
+        chunksize: int | None = None,
+    ) -> list[dict] | DataFrame | Iterator[list[dict]] | Iterator[DataFrame]:
         ...
 
     @classmethod
-    def as_records(cls, chunksize: int | None = None) -> list[dict]:
+    def read_sql(
+        cls,
+        sql: str,
+        as_format: str = "records",
+        chunksize: int | None = None,
+    ) -> list[dict] | DataFrame | Iterator[list[dict]] | Iterator[DataFrame]:
         ...
 
     @classmethod
-    def execute_sql(cls, sql: str, **params) -> InputTableMethods:
+    def get_current_table(cls) -> Table | None:
         ...
 
     @classmethod
-    def use_snapshot(cls, snapshot_name: str) -> InputTableMethods:
+    def exists(cls) -> bool:
         ...
 
 
 class OutputTableMethods:
     @classmethod
-    def write_records(cls, records: list[dict] | DataFrame, replace: bool = False):
+    def write(cls, records: DataFrame | list[dict] | dict, replace=False):
         ...
 
     @classmethod
-    def create_snapshot(cls, snapshot_name: str):
+    def execute_sql(cls, sql: str, created: bool = False):
         ...
 
     @classmethod
-    def execute_sql(cls, sql: str, **params):
+    def new_table(cls) -> Table:
+        ...
+
+    @classmethod
+    def get_current_table(cls) -> Table | None:
+        ...
+
+    @classmethod
+    def set_current_table(cls, table: Table):
+        ...
+
+    @classmethod
+    def signal_create(cls):
+        ...
+
+    @classmethod
+    def signal_update(cls):
+        ...
+
+    @classmethod
+    def exists(cls) -> bool:
         ...
 
 
@@ -46,7 +81,7 @@ class InputStreamMethods:
     def consume_records(cls, with_metadata: bool = False) -> Iterator[dict]:
         ...
 
-    def __iter__(self) -> Iterator[dict]:
+    def __iter__(cls) -> Iterator[dict]:
         ...
 
     @classmethod
@@ -57,30 +92,45 @@ class InputStreamMethods:
     def rollback(cls, records_to_rollback: int = 1):
         ...
 
+    @classmethod
+    def consume_all(cls) -> Iterator[str | None]:
+        ...
+
 
 class OutputStreamMethods:
     @classmethod
-    def stream_record(cls, record: dict):
+    def write(cls, record: DataFrame | list[dict] | dict):
         ...
 
 
 class StateMethods:
     @classmethod
-    def set_state(cls, state: dict):
+    def set(cls, state: dict):
         ...
 
     @classmethod
-    def set_state_value(cls, key: str, value: Any):
+    def set_value(cls, key: str, value: Any):
         ...
 
     @classmethod
-    def get_state(cls) -> dict:
+    def get(cls) -> dict:
         ...
 
     @classmethod
-    def get_state_value(cls, key: str) -> Any:
+    def get_value(cls, key: str) -> Any:
         ...
 
+    @classmethod
+    def should_continue(
+        cls, pct_of_limit: float = None, seconds_till_limit: int = None
+    ) -> bool:
+        ...
+
+    @classmethod
+    def request_new_run(
+        cls, trigger_downstream: bool = True, wait_atleast_seconds: int = None
+    ):
+        ...
 
 class ParameterMethods:
     pass
