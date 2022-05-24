c15877467575e579c2fc7af61d52eb3a4647c72f
Ken Van Haren <kenvanharen@gmail.com>
2022-05-24 11:18:44 -0700

    Fix test

--- patterns/cli/commands/upload.py
+++ patterns/cli/commands/upload.py
@@ -39,7 +39,9 @@ def upload(
     graph_version_id = resp["uid"]
     ui_url = resp["ui_url"]
     sprint(f"\n[success]Uploaded new graph version with id [b]{graph_version_id}")
-    errors = resp["manifest"].get("errors", [])
+    errors = resp.get("errors", [])
+    if resp.get("manifest"):
+        errors = errors + resp["manifest"].get("errors", [])
     if publish_component:
         errors = [
             e
