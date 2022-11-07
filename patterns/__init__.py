b781ee1e295a2f295956059ac2662877d8cad415
Ken Van haren <kenvanharen@gmail.com>
2022-11-07 15:35:47 -0800

    Remove legacy objects

--- patterns/__init__.py
+++ patterns/__init__.py
@@ -1,9 +1,4 @@
 from .node.node import (
-    node,
-    InputTable,
-    OutputTable,
-    InputStream,
-    OutputStream,
     Parameter,
     State,
     Stream,
@@ -13,11 +8,6 @@ from .node.node import (
 
 
 __all__ = [
-    "node",
-    "InputTable",
-    "OutputTable",
-    "InputStream",
-    "OutputStream",
     "Parameter",
     "State",
     "Table",
