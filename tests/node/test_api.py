7e47ca404e12fcf4f97c3aae9e20bef9223f833c
AJ <aj@alt>
2022-11-16 16:04:44 -0800

    Update imports

--- tests/node/test_api.py
+++
@@ -1,9 +0,0 @@
-from patterns import OutputTable, InputTable, Parameter, OutputStream, InputStream
-
-
-def test_methods():
-    OutputTable(description="description", schema="Schema")
-    InputTable(description="description", schema="Schema", required=False)
-    OutputStream(description="description", schema="Schema")
-    InputStream(description="description", schema="Schema", required=False)
-    Parameter(type=str, description="description", default="param")
