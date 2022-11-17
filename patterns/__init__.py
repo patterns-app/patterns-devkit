1db51511bfc6c5a958687700ac81076af7322298
AJ Alt <ajalt@users.noreply.github.com>
2022-11-17 09:01:47 -0800

    Remove deprecated methods from type stubs (#209)

--- patterns/__init__.py
+++ patterns/__init__.py
@@ -1,26 +1,14 @@
 from .node.node import (
-    node,
-    InputTable,
-    OutputTable,
-    InputStream,
-    OutputStream,
     Parameter,
     State,
-    Stream,
     Table,
     Connection,
 )
 
 
 __all__ = [
-    "node",
-    "InputTable",
-    "OutputTable",
-    "InputStream",
-    "OutputStream",
     "Parameter",
     "State",
     "Table",
-    "Stream",
     "Connection",
 ]
